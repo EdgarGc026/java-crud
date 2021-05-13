4354857af3c9ecca85ee8c4f7d173fbe0ef4e83b
edgargc026 <edgargc_26@hotmail.com>
2021-05-12 19:36:17 -0500

    Agregado un metodo para ver los datos en el table y en la bd

--- CRUD.java
+++ CRUD.java
@@ -1,6 +1,11 @@
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+import java.sql.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CRUD extends JFrame{
   private JPanel JPanel_myPanel;
@@ -26,6 +31,9 @@ public class CRUD extends JFrame{
     JTable JTable_myTable = new JTable(data, columnNames);
     JTable_myTable.setBounds(30,40,200,300);
     JTable_myTable.setCellSelectionEnabled(true);
+
+
+    //Eventos cada la celda individual
     ListSelectionModel JTable_select = JTable_myTable.getSelectionModel();
     JTable_select.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
     JTable_select.addListSelectionListener(new ListSelectionListener() {
@@ -45,6 +53,33 @@ public class CRUD extends JFrame{
     JScrollPane JScrollPane_myPane = new JScrollPane(JTable_myTable);
     JTable_myTable.setFillsViewportHeight(true);
     add(JScrollPane_myPane);
+  }
 
+  public void getData(){
+    try{
+      Conectar conectar = new Conectar();
+      Connection connection = conectar.conexion();
+
+      DefaultTableModel model = new DefaultTableModel();
+      model.addColumn("ID");
+      model.addColumn("Jugador1");
+      model.addColumn("Jugador2");
+      model.addColumn("Ganador");
+      String[] data = new String[4];
+
+      Statement statement = connection.createStatement();
+      ResultSet resultSet = statement.executeQuery("SELECT * FROM juego");
+      while (resultSet.next()){
+        data[0] = resultSet.getString(1);
+        data[1] = resultSet.getString(2);
+        data[2] = resultSet.getString(3);
+        data[3] = resultSet.getString(4);
+        model.addRow(data);
+      }
+      JTable_myTable.setModel(model);
+    }catch(Exception ex){
+      System.out.println("Error: "+ ex.getMessage());
+      System.out.println("Error a la hora de mostrar los datos");
+    }
   }
 }
